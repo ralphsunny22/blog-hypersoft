e1ad93e2317bfef85928eb4d4ca7c40811c611bc
Ugo Sunday Raphael <ralphsunny114@gmail.com>
2023-05-01 21:01:46 +0100

    controllers, routes setup

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from "express"
+import bodyParser from "body-parser"
+import postRoutes from "./routes/posts.js"
 
 const app = express();
 
-app.use(express.json) //so dat we can send data to our db
+app.use(express.json()) //so dat we can send data to our db
+//app.use(bodyParser.json) 
 
-app.listen(8000, ()=>{
-    console.log("Connected!")
+app.use("/api/posts", postRoutes)
+
+const port = 5000;
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
 })
